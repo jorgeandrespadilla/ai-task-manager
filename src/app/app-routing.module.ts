abc1f34e40a980432c7633d5ac23906f532d45ec
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2023-12-17 18:12:22 -0500

    feat: add app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'tasks',
+    loadChildren: () => import('./features/tasks/tasks.module').then(m => m.TasksModule)
+  },
+  {
+    path: 'categories',
+    loadChildren: () => import('./features/categories/categories.module').then(m => m.CategoriesModule)
+  },
+  { path: '**', redirectTo: 'tasks' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
