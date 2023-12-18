a95c587ed0a6c33312e8ec7df0d80508af8a3de7
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2023-12-18 16:31:54 -0500

    feat: update app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './core/layouts/main-layout/main-layout.component';
 
 const routes: Routes = [
   {
-    path: 'tasks',
-    loadChildren: () => import('./features/tasks/tasks.module').then(m => m.TasksModule)
-  },
-  {
-    path: 'categories',
-    loadChildren: () => import('./features/categories/categories.module').then(m => m.CategoriesModule)
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'tasks',
+        loadChildren: () => import('./features/tasks/tasks.module').then(m => m.TasksModule)
+      },
+      {
+        path: 'categories',
+        loadChildren: () => import('./features/categories/categories.module').then(m => m.CategoriesModule)
+      },
+      {
+        path: '**',
+        redirectTo: 'tasks'
+      }
+    ],
   },
   { path: '**', redirectTo: 'tasks' }
 ];
