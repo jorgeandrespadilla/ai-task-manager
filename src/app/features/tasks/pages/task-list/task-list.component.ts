1e292aed60e344636e3cd70b0849618ff1a966a0
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2023-12-24 12:58:11 -0500

    feat: add base search filters for tasks

--- src/app/features/tasks/pages/task-list/task-list.component.ts
+++ src/app/features/tasks/pages/task-list/task-list.component.ts
@@ -1,18 +1,23 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
 import { TasksService } from '../../../../services/tasks/tasks.service';
-import { tap } from 'rxjs';
+import { map, tap } from 'rxjs';
 import { TasksDetailService } from '../../../../services/tasks/tasks-detail.service';
 import { TaskStatusChange } from '../../../../shared/models/task.model';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+
+  filterForm = this.fb.group({
+    name: ['']
+  });
 
   selectedTaskId?: string;
 
@@ -20,15 +25,36 @@ export class TaskListComponent {
   confirmDeletion!: ElementRef;
 
   constructor(
+    private fb: FormBuilder,
     private router: Router,
+    private activatedroute: ActivatedRoute,
     private toastService: ToastrService,
     private modalService: NgbModal,
     private tasksService: TasksService,
     private tasksDetailService: TasksDetailService,
   ) { }
 
+  ngOnInit(): void {
+    this.activatedroute.queryParams.subscribe(params => {
+      this.filterForm.patchValue({
+        name: params['name'] ?? ''
+      });
+    });
+    this.filterForm.valueChanges.subscribe(() => {
+      this.updateQueryParams();
+    });
+  }
+
   get tasks$() {
-    return this.tasksDetailService.getTasks();
+    return this.tasksDetailService.getTasks().pipe(
+      map(tasks => tasks
+        .filter(task => task.name.includes(this.filterName))
+      )
+    );
+  }
+
+  get filterName() {
+    return this.filterForm.value.name ?? '';
   }
 
   addTask() {
@@ -70,6 +96,15 @@ export class TaskListComponent {
         this.cancelSelection();
       })
     ).subscribe();
+  }
+
+  private updateQueryParams() {
+    this.router.navigate([], {
+      queryParams: {
+        name: this.filterName === '' ? null : this.filterName
+      },
+      queryParamsHandling: 'merge'
+    });
   }
 
 }
